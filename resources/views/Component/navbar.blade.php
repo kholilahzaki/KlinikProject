00902bd62ef6ef91e3285d997a6fa28516a58f67
vaniameith <vameitaqiyyah@gmail.com>
2023-11-10 21:52:04 +0700

    update: nav use boostraps

--- resources/views/Component/navbar.blade.php
+++ resources/views/Component/navbar.blade.php
@@ -3,26 +3,35 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
         <!-- css -->
         <link rel="stylesheet" href="/css/style.css">
         <link rel="stylesheet" href="/css/navbar.css">
     </head>
 
     <body>
-        <nav>
-            <h5 style="text-align: justify; color: #394E91;">Klinik Raycare</h5>
-            <div class="navbar-menu">
-                <div class="navbar-menu-content">
-                    <p class="body-medium menu">Dashboard</p>
+        <div class="container">
+            <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+                <div class="col-md-3 mb-2 mb-md-0">
+                    <a href="/" class="h5 d-inline-flex link-body-emphasis text-decoration-none" style="color: #394E91;">
+                        Klinik Raycare
+                    </a>
                 </div>
-                <div class="navbar-menu-content">
-                    <p class="body-medium menu">Acara</p>
+
+                <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="body-medium nav-link px-2" style="color: #394E91;">Dashboard</a></li>
+                    <li><a href="#" class="body-medium nav-link px-2" style="color: #394E91;">Acara</a></li>
+                    <li><a href="#" class="body-medium nav-link px-2" style="color: #394E91;">TPS</a></li>
+                </ul>
+
+                <div class="col-md-3 text-end">
+                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
                 </div>
-                <div class="navbar-menu-content">
-                    <p class="body-medium menu">TPS</p>
-                </div>
-            </div>
-        </nav>
+            </header>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 
 </html>
