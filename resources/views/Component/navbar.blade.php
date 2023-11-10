704ea821e55498fd0e85c0b3ddcb449cbd7dee63
vaniameith <vameitaqiyyah@gmail.com>
2023-11-10 19:17:02 +0700

    Update: add Navbar

---
+++ resources/views/Component/navbar.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- css -->
+        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="/css/navbar.css">
+    </head>
+
+    <body>
+        <nav>
+            <h5 style="text-align: justify; color: #394E91;">Klinik Raycare</h5>
+            <div class="navbar-menu">
+                <div class="navbar-menu-content">
+                    <p class="body-medium menu">Dashboard</p>
+                </div>
+                <div class="navbar-menu-content">
+                    <p class="body-medium menu">Acara</p>
+                </div>
+                <div class="navbar-menu-content">
+                    <p class="body-medium menu">TPS</p>
+                </div>
+            </div>
+        </nav>
+    </body>
+
+</html>
