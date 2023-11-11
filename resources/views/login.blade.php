109046aa86633341805dce97560a1435000aeb51
jigglyjiggle <kholilahlismia31@gmail.com>
2023-11-11 16:40:00 +0700

    fixed login background

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,15 +10,15 @@
         <link rel="stylesheet" href="/css/login.css">
 
         <style>
+            body { background-color: #394E91; }
         </style>
     </head>
 
     <body>
-        <div class="display-bg" style="width: 100%; height: 100%; background-image: url(background-image.png); flex-direction: column; justify-content: center; align-items: center; display: inline-flex">
-            <div >
-
-            </div>
+        <div>
+            
         </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
