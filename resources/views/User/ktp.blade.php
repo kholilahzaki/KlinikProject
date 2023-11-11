ffe7327b842cb7aa37eb8c7e1aefc4a31b9ce50f
vaniameith <vameitaqiyyah@gmail.com>
2023-11-11 14:33:32 +0700

    udpate: add ktp file

---
+++ resources/views/User/ktp.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- bootstraps -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+        <!-- css -->
+        <link rel="stylesheet" href="/css/styles.css">
+
+        <style>
+            a.nav-link {color: #394E91;}
+            /* lihat hover warnanya di figma: success */
+            a.nav-link:hover {color: #293A79;} 
+        </style>
+    </head>
+
+    <body>
+    <header>
+                    <div class="d-flex align-items-center justify-content-md-between py-3" style="padding-left:40px; padding-right:40px; background:#FFFFFF; box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.25);">
+                        <!-- <div>
+                            <a href="/" class="h5 nav-link" style="text-align:justify; color:#394E91;">Klinik Raycare</a>
+                        </div>
+                        <div class="d-flex justify-content-center align-items-center gap-3" style="flex: 1 0 0;">
+                            <div class="d-flex justify-content-center align-items center align-self-stretch">
+                                <a href="#" class="nav-link body-medium text-align-justify" style="color:#394E91;">Dashboard</a>
+                            </div>
+                            <div class="d-flex justify-content-center align-items center align-self-stretch">
+                                <a href="#" class="nav-link body-medium text-align-justify" style="color:#394E91;">Acara</a>
+                            </div>
+                            <div class="d-flex justify-content-center align-items center align-self-stretch">
+                                <a href="#" class="nav-link body-medium text-align-justify" style="color:#394E91;">TPS</a>
+                            </div>
+                        </div>
+                        <div>
+                            <img src="https://github.com/mdo.png" alt="" width="40" height="40" class="rounded-circle me-2">
+                        </div> -->
+                        <div class="col-md-3 mb-2 mb-md-0">
+                            <a href="/" class="h5 d-inline-flex link-body-emphasis text-decoration-none" style="color: #394E91;">
+                                Klinik Raycare
+                            </a>
+                        </div>
+
+                        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                            <li><a class="body-medium nav-link px-2" href="#">Dashboard</a></li>
+                            <li><a class="body-medium nav-link px-2"  href="#">Acara</a></li>
+                            <li><a class="body-medium nav-link px-2" href="#">TPS</a></li>
+                        </ul>
+
+                        <div class="col-md-3 text-end">
+                            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                        </div>
+                    </div>
+                </header>
+                <div style="width: 100%; height: 100%; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+    <div style="border-radius: 10px; justify-content: center; align-items: center; gap: 4px; display: inline-flex">
+        <div style="width: 24px; height: 24px; position: relative">
+            <div style="width: 15.58px; height: 15.17px; left: 4.42px; top: 4.41px; position: absolute; background: #394E91"></div>
+        </div>
+        <div style="text-align: justify; color: #394E91; font-size: 16px; font-family: Inter; font-weight: 600; line-height: 24px; word-wrap: break-word">Kembali ke Halaman Utama</div>
+    </div>
+    <div style="justify-content: center; align-items: center; display: inline-flex">
+        <div style="text-align: justify; color: #1D1B20; font-size: 40px; font-family: Inter; font-weight: 700; line-height: 56px; word-wrap: break-word">Tambah Pengguna</div>
+    </div>
+</div>
+    </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Component/navbar');
 });
+
+Route::get('/ktp', function () {
+    return view('User/ktp');
+});
