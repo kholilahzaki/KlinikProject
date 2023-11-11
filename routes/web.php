263feef0d8eeab083a1bca419f929b85282a63c9
jigglyjiggle <kholilahlismia31@gmail.com>
2023-11-11 15:01:49 +0700

    add login route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function () {
+    return view('User/login');
+});
+
 Route::get('/', function () {
     return view('Component/navbar');
 });
