440662a282dcf0d9741d25e05fc43dbf7a05541e
jigglyjiggle <kholilahlismia31@gmail.com>
2023-11-11 15:20:06 +0700

    try to fix login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- bootstraps -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+        <!-- css -->
+        <link rel="stylesheet" href="/css/login.css">
+
+        <style>
+        </style>
+    </head>
+
+    <body>
+        <div class="display-bg" style="width: 100%; height: 100%; background-image: url(background-image.png); flex-direction: column; justify-content: center; align-items: center; display: inline-flex">
+            <div >
+
+            </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
+</html>
