f71dc54c6c0aabce48fb443305438dfa8029c91f
asviananda <oktarizkasviananda@gmail.com>
2023-11-14 15:27:05 +0700

    new migration, controller, model added

---
+++ app/Lc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lc extends Model
+{
+    use HasFactory;
+}
---
+++ app/Penarikan_lc.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Penarikan_lc extends Model
+{
+    use HasFactory;
+}
